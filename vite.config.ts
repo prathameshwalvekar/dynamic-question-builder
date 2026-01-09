669ad5a8b4c558c240b5507af941a6aaea5da665
prthamesh177 <walvekarprathamesh734@gmail.com>
2026-01-09 17:46:29 +0530

    vite conflll

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
@@ -16,6 +11,6 @@ export default defineConfig(({ mode }) => ({
     },
   },
   build: {
-    outDir: "build", // Output to 'build' to match Render's default
+    outDir: "build",
   },
 }));
